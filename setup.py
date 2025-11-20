3b50461a5ff6ee2fbd95a2fb9da088064868a45e
Tanisha Rao <tanisharao0704@gmail.com>
2025-11-20 17:27:06 +0530

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sensor',
+    version='0.0.1',
+    author='tanisha',
+    author_email='tanisharao0704@gmail.com',
+    packages=find_packages(),
+)
